ee088521f3ff10ca6cf2db40e2ec1d186abd92d6
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-13 02:41:45 +0200

    feat: key exchange interface, attach security services to secure socket

---
+++ SafeChat/Components/Services/SocketServices/Security/CustomKeyExchangeService.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SafeChat
+{
+    public class CustomKeyExchangeService : KeyExchangeService
+    {
+        public override Task<string> DecryptSessionKey(string encryptedSessionKey) => throw new NotImplementedException();
+        public override Task<string> EncryptSessionKey(string sessionKey) => throw new NotImplementedException();
+        public override Task GenerateKeyPairs() => throw new NotImplementedException();
+        public override Task<string> GenerateSessionKey() => throw new NotImplementedException();
+        public override Task<string> GetPublicKey() => throw new NotImplementedException();
+        public override Task SetRemotePublicKey(string publicKey) => throw new NotImplementedException();
+    }
+}
--- SafeChat/Components/Services/SocketServices/Security/SecureSocketServiceDecorator.cs
+++ SafeChat/Components/Services/SocketServices/Security/SecureSocketServiceDecorator.cs
@@ -3,6 +3,15 @@ namespace SafeChat
 {
     public class SecureSocketServiceDecorator : SocketServiceDecorator
     {
-        public SecureSocketServiceDecorator(SocketService socketService) : base(socketService) { }
+        private readonly KeyExchangeService _keyExchangeService;
+        private readonly EncryptionService _encryptionService;
+        private readonly SignatureService _signatureService;
+
+        public SecureSocketServiceDecorator(SocketService socketService) : base(socketService)
+        {
+            _keyExchangeService = new CustomKeyExchangeService();
+            _encryptionService = new CustomEncryptionService();
+            _signatureService = new CustomSignatureService();
+        }
     }
 }
